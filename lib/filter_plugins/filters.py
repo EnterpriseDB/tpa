a0b2a7f0488bb24b8447acd71839a99518a992a4
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2015-07-17 12:18:12 +0530

    Ansible can't |map(attribute='x'); here's a simple-minded substitute for assertions

--- lib/filter_plugins/filters.py
+++ lib/filter_plugins/filters.py
@@ -1,6 +1,22 @@
+from jinja2.filters import contextfilter
+
 def merge(a, b):
-   return dict(a.items() + b.items());
+    return dict(a.items() + b.items());
+
+# This is a poor substitute for map(attribute='x'), which Ansible does
+# not currently support.
+
+@contextfilter
+def oneattr(context, seq, attr):
+    func = lambda x: context.environment.getitem(x, attr)
+
+    l = []
+    if seq:
+        for item in seq:
+            l += func(item)
+
+    return l
 
 class FilterModule(object):
-   def filters(self):
-     return { 'merge' : merge }
+    def filters(self):
+        return { 'merge' : merge, 'oneattr' : oneattr }
