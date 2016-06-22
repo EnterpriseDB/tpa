77e72103f4757acf95e53d5c976ecb7e1e117c11
Nikhil Sontakke <nikhils@2ndQuadrant.com>
2016-06-22 07:54:18 +0000

    1) Use "assign_eip" as true to assign an elastic IP to the specific instance.    Committing with assign_eip with "false" for now in the config.yml
    2) Install "boto" on the instances. This is needed for running the eip
       failover script
    
    3) Modify the promote_command.sh file. If the "repmgr promote" command
       succeeds, only then do we invoke the python script to dissociate the
       eip from the old master and associate it with itself
    
    4) A new template file assign_eip.py.j2 which gets copied to the standby
       instances after replacing the instance ID and elastic ip values
       appropriately. It gets triggered as mentioned in 3 above.

--- CustomCloud/roles/repmgr/common/files/promote_command.sh
+++ CustomCloud/roles/repmgr/common/files/promote_command.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 repmgr standby promote -f /etc/repmgr/repmgr.conf
+
+# On successful promotion, failover the elastic IP to itself
+
+if [ "$?" -eq "0" ]; then
+	python2.7 /etc/repmgr/assign_eip.py
+fi
