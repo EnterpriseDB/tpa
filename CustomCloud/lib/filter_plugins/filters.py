75d4460372d493472d041475cb58dd18b10fee2d
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2016-08-25 22:49:27 +0530

    Remove our local copy of the extract filter
    I added it when it hadn't yet been committed upstream.

--- CustomCloud/lib/filter_plugins/filters.py
+++ CustomCloud/lib/filter_plugins/filters.py
@@ -39,24 +39,6 @@ def define_volumes(v, root_device_name):
                 i['device_name'] = root_device_name
     return v
 
-# This filter takes an item and a container, and returns the value of the item
-# in the container (i.e. a key in a dict or an index in a list). It optionally
-# takes an array of keys to look up recursively in the first result:
-#
-# ['a', 'b']|map('extract', {'a': 21, 'b': 42})|list
-# groups['x']|map('extract', hostvars, 'ec2_id')|list
-
-def extract(item, container, morekeys=None):
-    value = container[item]
-
-    if value is not Undefined and morekeys is not None:
-        if not isinstance(morekeys, list):
-            morekeys = [morekeys]
-
-        value = reduce(lambda d, k: d[k], morekeys, value)
-
-    return value
-
 # Based on PR ansible/ansible#11083, this filter takes a container and a subkey
 # ('x.y.z', or [x,y,z]) and a default value, and returns container.x.y.z or the
 # default value if any of the levels is undefined.
@@ -77,7 +59,6 @@ def try_subkey(container, keys, default=None):
 class FilterModule(object):
     def filters(self):
         return {
-            'extract': extract,
             'expand_instances': expand_instances,
             'try_subkey': try_subkey,
             'define_volumes': define_volumes,
