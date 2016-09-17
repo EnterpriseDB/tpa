c0270472418a906eb0836dc51dd7c8ca1f0f5d66
Devashish Pandey <devashish.pandey@2ndquadrant.com>
2016-09-17 11:04:26 +0530

    WebUI: Added new model for a queue that handles service creation jobs.

--- web/pgadmin/model/__init__.py
+++ web/pgadmin/model/__init__.py
@@ -81,9 +81,41 @@ class Setting(db.Model):
     setting = db.Column(db.String(256), primary_key=True)
     value = db.Column(db.String(1024))
 
+class Queue(db.Model):
+    """Define a Queue"""
+    __tablename__ = 'queue'
+    queue_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        nullable=False
+    )
+    service_id = db.Column(
+        db.Integer,
+        db.ForeignKey('service.id'),
+        nullable=False
+    )
+    service_name = db.Column(db.String(128), nullable=False)
+    queued_at = db.Column(
+        db.DateTime,
+        nullable=False
+    )
+    dequeued_at = db.Column(
+        db.DateTime,
+        nullable=True
+    )
+    completed_at = db.Column(
+        db.DateTime,
+        nullable=True
+    )
+    failed_at = db.Column(
+        db.DateTime,
+        nullable=True
+    )
+
 
 class Service(db.Model):
-    """Define a registered Postgres Cluster"""
+    """Define a registered Service"""
     __tablename__ = 'service'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(
