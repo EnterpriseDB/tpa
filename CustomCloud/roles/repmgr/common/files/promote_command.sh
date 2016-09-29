cdafbb79d54fd81691b4b770bace3e01a9da2827
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2016-09-29 11:18:47 +0530

    Move repmgr configuration to /etc/repmgr/9.5
    That's where it is under RHEL, and we can save some trouble if we use
    the same thing on Debian (because /etc/repmgr/repmgr.conf wasn't the
    standard location on Debian anyway).

--- CustomCloud/roles/repmgr/common/files/promote_command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-repmgr standby promote -f /etc/repmgr/repmgr.conf
-
-# On successful promotion, failover the elastic IP to itself
-
-if [ "$?" -eq "0" ]; then
-	python2.7 /etc/repmgr/assign_eip.py
-fi
