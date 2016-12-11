ca69fdc7fbf5f83bdcbeaa3ff3b9cc401e9ca738
Sirtaj Singh Kang <sirtaj@2ndQuadrant.com>
2016-12-11 13:53:58 +0530

    Moved js entry point and requirements to main.js.

--- ui/fe/js/main.js
+++ ui/fe/js/main.js
@@ -1,8 +1,25 @@
 
 // vim:ts=4:sts=4:sw=4:et:ff=unix:fileencoding=utf-8
+//
 
-/**
- * 
- */
+var d3 = require("d3");
 
-require('d3-selection')
+
+/*
+require("./diagram");
+require("./utils");
+*/
+
+var tpa = require("./tpa-api");
+var tpa_d3 = require("./tpa-d3");
+
+// XXX This is for testing the cluster diagram.
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    var next_cluster = tpa_d3.show_clusters(tpa.TEST_TENANT,
+                                    d3.select(".cluster_view"),
+                                    1000, 1000);
+    d3.select(".fa-plus")
+        .on("click", function(d,i) { next_cluster(); })
+        .each(console.log);
+});
