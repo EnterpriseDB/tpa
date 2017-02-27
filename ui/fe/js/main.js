178bb303887f649e83a6ef794a9f645f6aea97a1
Sirtaj Singh Kang <sirtaj@2ndQuadrant.com>
2017-02-27 14:05:01 +0530

    Don't use js code in the HTML files, all entry points are matched in main.js.

--- ui/fe/js/main.js
+++ ui/fe/js/main.js
@@ -12,99 +12,74 @@ import * as tpa_diagram from "./tpa-d3";
 import * as d3 from "d3";
 import {get_url_vars} from "./utils";
 
-export function require_login() {
-    api.auth.popup_login();
-}
-
-export function display_all_cluster_diagrams() {
-    document.addEventListener("DOMContentLoaded", function(e) {
-        var next_cluster = tpa_diagram.show_clusters(
-                d3.select(".cluster_view"));
-        d3.select("button.next-cluster").on("click", () => next_cluster());
-    });
-}
-
-
-export function display_cluster_diagram(viewport_class) {
-    document.addEventListener("DOMContentLoaded", function(e) {
+function display_cluster_diagram() {
+    const viewport = d3.select(".cluster_view");
+    if (!viewport.empty()) {
         let vars = get_url_vars();
 
-        if (vars.cluster) { 
+        if (vars.cluster) {
             d3.select("button.next-cluster").style("visibility", "hidden");
             tpa_diagram.display_cluster_by_uuid(vars.cluster,
-                    d3.select(viewport_class));
+                    viewport);
         }
         else {
-            var next_cluster = tpa_diagram.show_clusters(d3.select(viewport_class));
+            var next_cluster = tpa_diagram.show_clusters(viewport);
             d3.select("button.next-cluster").on("click", () => next_cluster());
         }
+    }
+}
 
+
+function do_login() {
+    d3.selectAll("form.login-form").on("submit", () => {
+        let username = d3.select("input.username").node().value;
+        let password = d3.select("input.password").node().value;
+
+        d3.event.preventDefault();
+
+        api.auth.login(username, password, function(error, result) {
+            console.log("Login! error", error, "result", result);
+            if (!error) {
+                window.location = "/cluster.html";
+            }
+            else {
+                // error
+                alert("Login failed, please check your credentials.");
+            }
+        });
+
+        return true;
     });
 }
 
+function submit_cluster_upload() {
+    d3.selectAll("form.cluster_upload_yml").on("submit", () => {
+        let tenant = d3.select("input.tenant").node().value;
+        let config_yml = d3.select("input.config_yml").node().value;
 
-export function register_login(on_success) {
-    function do_login() {
-        var login_form = d3.select("form.login-form");
+        d3.event.preventDefault();
 
-        if (!login_form) {
-            return;
-        }
-
-        login_form.on("submit", () => {
-            let username = d3.select("input.username").node().value;
-            let password = d3.select("input.password").node().value;
-
-            d3.event.preventDefault();
-
-            api.auth.login(username, password, function(error, result) {
-                if (!error) {
-                    if (on_success) {
-                        on_success();
-                    }
-                    else {
-                        window.location = "/cluster.html";
-                    }
-                }
-                else {
-                    // error
-                    alert("Login failed, please check your credentials.");
-                }
-            });
-
-            return true;
+        api.cluster_upload(tenant, config_yml, function(error, res) {
+            if (!error) {
+                window.location = "/cluster.html?cluster=" + res.cluster;
+            }
+            else{
+                alert(error);
+            }
         });
-    }
-
-    document.addEventListener("DOMContentLoaded", do_login);
+        return true;
+    });
 }
 
-
-export function register_cluster_upload() {
-    function submit_cluster_upload() {
-        d3.select("form.cluster_upload_yml").on("submit", () => {
-            let tenant = d3.select("input.tenant").node().value;
-            let config_yml = d3.select("input.config_yml").node().value;
-
-            d3.event.preventDefault();
-
-            api.cluster_upload(tenant, config_yml, function(error, res) {
-                if (!error) {
-                    window.location = "/cluster.html?cluster=" + res.cluster;
-                }
-                else{
-                    alert(error);
-                }
-            });
-            return true;
-        });
-    }
-
-    document.addEventListener("DOMContentLoaded", submit_cluster_upload);
-}
-
+// Main entry point.
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Called");
     d3.selectAll("#cover").style("visibility", "hidden");
+    if(!d3.select("meta#login-required").empty()) {
+        console.log("Login required!");
+        api.auth.popup_login();
+    }
+    do_login();
+    display_cluster_diagram();
+    submit_cluster_upload();
 });
