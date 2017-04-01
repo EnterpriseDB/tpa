de5bba5d5c63594911b55a33595ed26cef605bc9
Sirtaj Singh Kang <sirtaj@2ndQuadrant.com>
2017-04-01 19:20:49 +0530

    Dev login data fixture.

--- ui/apps/tpa/tests/conftest.py
+++ ui/apps/tpa/tests/conftest.py
@@ -1,6 +1,10 @@
 import os
+
 import django
 from django.conf import settings
+from django.core.management import call_command
+
+import pytest
 
 
 # `pytest` automatically calls this function once when tests are run.
@@ -14,3 +18,11 @@ def pytest_configure():
     django.setup()
     # Note: In Django =< 1.6 you'll need to run this instead
     # settings.configure()
+
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', '100_site_owner_tenant.yaml')
+        call_command('loaddata', '300_archetype_tpa.yaml')
