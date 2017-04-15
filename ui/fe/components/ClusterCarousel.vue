7425635faa867a6719c8c44b528d05150840f773
Sirtaj Singh Kang <sirtaj@2ndQuadrant.com>
2017-04-15 22:10:30 +0530

    Fixed carousel element order and added diagram loading indicator.

--- ui/fe/components/ClusterCarousel.vue
+++ ui/fe/components/ClusterCarousel.vue
@@ -1,32 +1,34 @@
 <template>
-<div id="cluster-carousel" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-      <template v-for="(cluster, index) in clusters">
-        <li data-target="#cluster-carousel" :data-slide-to="index" :class="active(cluster)">
-      </li>
-      </template>
-  </ol>
+    <div id="cluster-carousel" class="carousel slide" data-ride="carousel">
 
-  <div class="carousel-inner" role="listbox">
-      <div v-for="cluster in clusters" :class="'item '+active(cluster)">
-          <div class="carousel_cluster_diagram cluster_diagram" :href="cluster.url">
-          </div>
-          <div class="carousel-caption">
-              <h3>{{ cluster.name }}</h3>
-              <p v-if="cluster.description">{{ cluster.description }}</p>
-          </div>
-      </div>
-  </div>
+        <div class="carousel-inner" role="listbox">
+            <div v-for="cluster in clusters" :class="'item '+active(cluster)">
+                <div class="carousel_cluster_diagram cluster_diagram" :href="cluster.url">
+                    <p>Loading...</p>
+                </div>
+                <div class="carousel-caption">
+                    <h3>{{ cluster.name }}</h3>
+                    <p v-if="cluster.description">{{ cluster.description }}</p>
+                </div>
+            </div>
+        </div>
 
-  <a class="left carousel-control" href="#cluster-carousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#cluster-carousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+        <ol class="carousel-indicators">
+            <template v-for="(cluster, index) in clusters">
+                <li data-target="#cluster-carousel" :data-slide-to="index" :class="active(cluster)">
+                </li>
+            </template>
+        </ol>
+
+        <a class="left carousel-control col-sm-2" href="#cluster-carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control col-sm-2" href="#cluster-carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </template>
 
 <script>
@@ -61,15 +63,19 @@ export default Vue.extend({
         });
     },
     updated() {
-        let containers = d3.select(this.$el).selectAll("div.carousel_cluster_diagram");
+        let containers = d3.select(this.$el)
+            .selectAll("div.carousel_cluster_diagram");
 
         containers.each(function() {
             let container = d3.select(this);
+            container.selectAll("p").remove();
             show_cluster_diagram(container, container.attr('href'));
         });
     },
     methods: {
         add_cluster(cluster) {
+            if (this.clusters.indexOf(cluster) >= 0) { return; }
+
             this.clusters.push(cluster);
             if(!this.current_cluster) {
                 this.current_cluster = cluster;
