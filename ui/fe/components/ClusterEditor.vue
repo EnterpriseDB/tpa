7627a3c7d58066e018f2787fc5285addc0517a06
Sirtaj Singh Kang <sirtaj@2ndQuadrant.com>
2017-04-18 04:04:45 +0530

    Added DetailPanel vue that uses existing d3 code.

--- ui/fe/components/ClusterEditor.vue
+++ ui/fe/components/ClusterEditor.vue
@@ -1,12 +1,15 @@
 
 <template>
-<cluster-diagram :url="url"></cluster-diagram>
+<cluster-diagram :url="url" @selected="set_panel_detail">
+</cluster-diagram>
 </template>
 
 <script>
 
+import * as d3 from "d3";
 import Vue from "vue";
 import ClusterDiagram from "./ClusterDiagram.vue";
+import DetailPanel from "./DetailPanel.vue";
 import * as api from "../js/tpa-api";
 
 export default Vue.extend({
@@ -14,8 +17,26 @@ export default Vue.extend({
     data: () => ({
         url: api.window_model().api_url
     }),
+    created() {
+        // spawn detail panel outside the main_content container.
+        d3.select("body").append("div").attr("id", "detail-panel");
+        this.detail_panel = new DetailPanel({
+            el: "#detail-panel",
+            master: this.url,
+            detail: this.url,
+        });
+    },
+    mounted() {
+        this.set_panel_detail(this.url);
+    },
     components: {
-        ClusterDiagram
+        ClusterDiagram,
+        DetailPanel,
+    },
+    methods: {
+        set_panel_detail(obj) {
+            this.detail_panel.detail = obj ? obj : this.url;
+        }
     }
 });
 
