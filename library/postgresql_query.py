86cd998213cad1f3e1030c637047c69c4fd0ed1f
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2017-08-25 10:02:12 +0530

    Adjust boilerplate
    I don't know what any of this __future__ stuff actually is good for, but
    it seems to be the recommended practice these days.

--- library/postgresql_query.py
+++ library/postgresql_query.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 DOCUMENTATION = '''
 ---
