b1fccec31dc450f203eb67674f6bcf5559344895
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2017-09-01 14:41:05 +0530

    Add more comments
    Also, don't fail when we can't connect to postgres.

--- library/cluster_discovery.py
+++ library/cluster_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# This module expects to be invoked on an instance «with: 'postgres' in role».
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -29,15 +31,13 @@ EXAMPLES = '''
 - name: Collect facts about the Postgres cluster
   cluster_discovery:
     conninfo: dbname=postgres
+  become_user: "{{ postgres_user }}"
+  become: yes
   register: p
 - debug: msg="the data directory is {{ p.postgres_data_dir }}"
 '''
 
-import io
-import os
-import sys
-import pwd
-import grp
+import os, io, sys, pwd, grp
 
 try:
     import psycopg2
@@ -48,11 +48,6 @@ else:
     psycopg2_found = True
 
 def main():
-
-    # We are passed in all of the overridable values set by postgres/vars, and
-    # have to test to see if they match this instance in reality. Then we can
-    # query the server and discover its role in the cluster.
-
     module = AnsibleModule(
         argument_spec=dict(
             conninfo=dict(default=""),
@@ -69,10 +64,27 @@ def main():
     # suitable conninfo string and invoke this module as a user that can use it
     # to connect.
 
+    conn = None
     conninfo = module.params['conninfo']
     try:
         conn = psycopg2.connect(dsn=conninfo)
         cur = conn.cursor()
+
+        # First, we discover postgres_version and its variants.
+
+        cur.execute('SELECT version()')
+        m['postgres_version_string'] = cur.fetchone()[0]
+        m['postgres_version_int'] = conn.server_version
+        m['postgres_version'] = '%d.%d' % (
+            (conn.server_version/10000)%10,
+            (conn.server_version/100)%10,
+        )
+
+        # We collect and return everything in pg_settings, and use that to also
+        # fill in postgres_data_dir and postgres_port. (There's no easy way to
+        # discover postgres_host, but we wouldn't have been able to connect to
+        # Postgres in the first place if we didn't have a pretty good idea of
+        # what it was set to.)
 
         m['pg_settings'] = settings = {}
         cur.execute('SELECT name,setting FROM pg_settings')
@@ -82,14 +94,10 @@ def main():
         m['postgres_port'] = int(settings['port'])
         m['postgres_data_dir'] = settings['data_directory']
 
-        cur.execute('SELECT version()')
-        m['postgres_version_string'] = cur.fetchone()[0]
-        m['postgres_version_int'] = conn.server_version
-        m['postgres_version'] = '%d.%d.%d' % (
-            (conn.server_version/10000)%10,
-            (conn.server_version/100)%10,
-            (conn.server_version)%10,
-        )
+        # A backend pid leads us to /proc/<pid>/{exe,status}, whence we can find
+        # postgres_bin_dir, postgres_user, postgres_group, and postgres_home. We
+        # assume that we are either running as root, or have permission to read
+        # this because we're running as the same user as postgres.
 
         cur.execute('SELECT pg_backend_pid()')
         pid = cur.fetchone()[0]
@@ -108,16 +116,20 @@ def main():
                     ent = grp.getgrgid(int(s[1]))
                     m['postgres_group'] = ent.gr_name
 
+        # We're done with the basic system facts, so we move on to querying the
+        # server to get an idea of its place in the world^Wcluster.
+
+        m['query'] = query = {}
         cur.execute('SELECT pg_is_in_recovery()')
-        m['postgres_is_in_recovery'] = cur.fetchone()[0]
+        query['pg_is_in_recovery'] = cur.fetchone()[0]
 
     except Exception as e:
         m['error'] = str(e)
+        if conn is not None:
+            m['failed'] = 'error' in m
 
     # If we can't connect, all we can do is to check a few likely
     # places to see if we can find matches for the basic OS-level facts.
-
-    m['failed'] = 'error' in m
 
     module.exit_json(**m)
 
