8c364e7f3d96c0b02bde8c37424ec7d7e5c88509
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2018-06-26 22:14:53 +0530

    Move image setting into configure-defaults
    Every configure script does exactly the same thing anyway.

--- architectures/lib/configure-defaults.sh
+++ architectures/lib/configure-defaults.sh
@@ -6,3 +6,6 @@ instance_type=${instance_type:-t2.micro}
 if [[ ${distribution:=Debian} != *-minimal ]]; then
     distribution="$distribution${minimal:-}"
 fi
+
+image=$($libdir/image $distribution $platform $architecture)
+eval $image
