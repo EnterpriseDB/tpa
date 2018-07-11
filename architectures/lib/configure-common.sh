0ebfa86bd5f927eb5516f96863abe58d74f39036
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2018-07-11 15:18:20 +0530

    Clarify error if you run architectures/x/configure by hand

--- architectures/lib/configure-common.sh
+++ architectures/lib/configure-common.sh
@@ -4,7 +4,7 @@ set -eu
 archdir=$(dirname $0)
 libdir=$(dirname $0)/../lib
 
-cluster=${1:?"No cluster directory specified (please run this script via 'tpaexec config clustername …')"}
+cluster=${1:?"No cluster directory specified (please run 'tpaexec configure clustername …' instead)"}
 shift
 
 error() {
