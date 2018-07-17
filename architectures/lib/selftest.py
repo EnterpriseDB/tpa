c3ae41e7a81abecb3f886d94e67534717c480a4a
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2018-07-17 16:34:55 +0530

    Add selftest.py that checks module imports
    We don't check for all of the modules we require yet (e.g., boto), but
    this gives a more useful error message (than an Ansible stack trace)
    when basic modules like jinja2/yaml are missing.

---
+++ architectures/lib/selftest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import importlib
+
+want = [
+    'jinja2'
+    , 'yaml'
+    , 'cryptography'
+    , 'dateutil'
+    , 'setuptools'
+    , 'netaddr'
+    , 'six'
+]
+have = []
+
+for m in want:
+    try:
+        importlib.import_module(m)
+        have.append(m)
+    except:
+        pass
+
+if len(have) < len(want):
+    missing = [x for x in want if x not in have]
+    print('ERROR: missing Python modules: ' + ', '.join(missing))
+    if not hasattr(sys, 'real_prefix'):
+        print('NOTE: %s is not running under a virtualenv' % sys.executable)
+    sys.exit(-1)
+
+sys.exit(0)
