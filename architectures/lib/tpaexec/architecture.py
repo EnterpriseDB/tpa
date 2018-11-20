98d14438f396c70374d9ec0ddd8e921b34ab785b
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2018-11-20 12:06:01 +0530

    Set repmgr_package_version to 4.0.6* only for M1
    There are no repmgr 4.0.6 packages for Postgres 11, so we don't want to
    force the package version gratuitously, especially for architectures
    like BDR-Simple (which don't currently need repmgr the way M1 does).
    
    Reported by Gianni, Amruta, and others.

--- architectures/lib/tpaexec/architecture.py
+++ architectures/lib/tpaexec/architecture.py
@@ -143,7 +143,6 @@ class Architecture(object):
             'root_volume_size': 16,
             'barman_volume_size': 32,
             'postgres_volume_size': 16,
-            'repmgr_package_version': '4.0.6*',
         }
 
     # Returns the default platform for this architecture (i.e., the platform
