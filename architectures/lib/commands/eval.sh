db251dfc8fafcc24f00aedd3efef4edeaf7cfae3
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2019-02-07 10:06:39 +0530

    Add an experimental ``tpaexec eval`` command
    This just does ``debug: msg="{{ expression }}"``, but it does it after
    running init.yml, so that all the basic stuff we expect to be defined in
    a deployment playbook is already in place, which is not the case if you
    run ``tpaexec cmd -m debug …``.
    
    You can run it like this:
    
        tpaexec eval . "{{ some expression }}"
    
        # Don't include roles/init
        tpaexec eval . "{{ some expression }}" --no-init
    
        # Run only on specified hosts
        tpaexec eval . "{{ some expression }}" --hosts foo,bar,baz
    
    Expression must come first, options afterwards.

---
+++ architectures/lib/commands/eval.sh
@@ -0,0 +1,42 @@
+# Copyright © 2ndQuadrant Limited <info@2ndquadrant.com>
+
+set -eu
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+    script=$(basename $0)
+    echo "ERROR: this script is meant to be executed with 'tpaexec ${script%.sh} …'" >&2
+    exit 1
+fi
+
+_tpaexec_command() {
+    expr=${1:?no expression specified}
+    shift
+
+    REMAINDER=()
+    while [[ $# -gt 0 ]]; do
+        opt=$1
+        shift
+
+        case "$opt" in
+            --hosts)
+                if [[ -z ${1:-''} ]]; then
+                    error "$opt: No hosts specified"
+                fi
+                REMAINDER+=(-e eval_hosts="$1")
+                shift
+                ;;
+
+            --no-init)
+                REMAINDER+=(-e init=no)
+                ;;
+
+            *)
+                REMAINDER+=("$opt")
+                ;;
+        esac
+    done
+
+    set -- "${REMAINDER[@]:+${REMAINDER[@]}}" "$@"
+
+    time playbook $TPA_DIR/architectures/lib/commands/eval.yml -e expr="$expr" "$@"
+}
