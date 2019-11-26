0de09da1ecb696985b0303fa697e1fba6b0f9655
Amruta Deolasee <amrutadeolasee@gmail.com>
2019-11-26 07:14:55 +0000

    Added a python script to get statistics continuosly
    Runtime checks as ansible tasks requires
    confirmed knowledge of origin and partner.
    These nodes may or may not switch their roles after failure
    So the test as anisble task did not work right
    
    The pythin script will run asynchronously fetching required info
    from camo pairs and generating final report as a csv file.
    
    We can further add tasks using this csv. Currently nothing added.

---
+++ roles/test/files/run_time_check.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+import psycopg2
+import csv
+import sys
+import time
+from datetime import datetime
+import os
+
+origin_dsn = sys.argv[1]
+partner_dsn = sys.argv[2]
+local_dsn = sys.argv[3]
+csvfile = sys.argv[4]
+test_table = sys.argv[5]
+conn_origin=0
+conn_partner=0
+# datetime object containing current date and time
+now = datetime.now()
+csvRow = [now]
+emptyRow = ["",  "", ""]
+myquery = "SELECT 1 FROM pg_catalog.pg_class c INNER JOIN pg_catalog.pg_namespace n ON (c.relnamespace = n.oid) WHERE relname ='{}'".format(test_table)
+if os.path.exists(csvfile):
+  os.remove(csvfile)
+# Generate a csv report of statistics collected
+def generate_report():
+    csv.register_dialect('mydialect',
+    delimiter = '|',
+    quoting=csv.QUOTE_NONE,
+    skipinitialspace=True)
+    with open(csvfile, "a") as fp:
+        wr = csv.writer(fp, dialect='mydialect')
+        wr.writerow(csvRow)
+    fp.close()
+
+def get_stats(conn):
+    try:
+        cur = conn.cursor()
+        cur.execute('SELECT interface_name from bdr.local_node_summary')
+        node_name = cur.fetchone()[0]
+        cur.execute('SELECT count(*) FROM camo_test')
+        xacts = cur.fetchone()[0]
+        cur.execute('SELECT COUNT(*) FROM pg_prepared_xacts')
+        prepared_xacts = cur.fetchone()[0]
+        newRow = [node_name, xacts, prepared_xacts]
+        csvRow.extend(newRow)
+        #closing database connnection.
+        cur.close()
+        conn.close()
+         
+    except:
+        print "Something wrong with the connection or queries"
+        csvRow.extend(emptyRow)
+
+
+
+# Get local connection
+# Continue to get statistics until tets table exists
+try:
+    conn_local=psycopg2.connect(local_dsn)
+    cur_local=conn_local.cursor()
+    cur_local.execute(myquery)
+    test_valid = cur_local.fetchone()[0]
+    cur_local.close()
+    conn_local.close()
+except:
+    test_valid = 0
+
+while test_valid:
+   
+    # Try to get connection to origin
+    try:
+        conn_origin=psycopg2.connect(origin_dsn, connect_timeout=5)
+    except:
+        print "I am unable to connnect to the ." ,origin_dsn
+        
+    # Try to get connection to partner
+    try:
+        conn_partner=psycopg2.connect(partner_dsn,connect_timeout=5)
+    except:
+        print "I am unable to connnect to the ." ,partner_dsn
+        
+    # If connection ito origin is established
+    # get statistics from the node else enter empty values
+    if(conn_origin):
+        get_stats(conn_origin)
+    else:
+        csvRow.extend(emptyRow)
+        
+    
+    # If connection to partner is established
+    # get statistics from the node else enter empty values
+    if(conn_partner):
+        get_stats(conn_partner)
+    else:
+        csvRow.extend(emptyRow)
+    
+    # Generate a csv report with above statistics
+    generate_report()
+    
+    # Continue until test runs. `camo_test` table is deleted at the end of the tests
+    retry_counter = 12
+    while True:
+        try:
+            conn_local=psycopg2.connect(local_dsn)
+            break
+        except:
+	    print "Retry establishing local connection"
+            time.sleep(5)
+	    retry_counter = retry_counter - 1
+        if(retry_counter == 0):
+	    print "Could not establish local connection. Exiting"
+	    sys.exit()
+        
+    cur_local=conn_local.cursor()
+    cur_local.execute(myquery)
+    table_exists=cur_local.fetchone()
+    if table_exists:
+        test_valid = 1
+        time.sleep(5)
+        now = datetime.now()
+        csvRow = [now]
+    else: 
+        cur_local.close()
+        conn_local.close()
+        sys.exit()
