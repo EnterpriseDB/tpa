a6a0fa19e8bc35995bcaf01c4b62fd6724792ccf
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2020-08-21 10:07:10 +0530

    Install ccache by default in postgres/src/req
    1. Install ccache by default for source builds
    
    2. Add the ccache directories to the default build PATH (for which we
       introduce a new ``build_path`` setting, which can be set like any
       other variable in config.yml)
    
    3. On Docker containers, we set up a named volume to serve as a shared
       ccache for the cluster's containers. Use the new --shared-ccache
       configure option to use a shared host directory instead.
    
    4. During deprovision, we also remove any volumes named ccache-xxx that
       are referenced by any instances in the cluster. (Of course, we don't
       touch any --shared-ccache directory, since the point of using that
       option is to be able to have a cache that outlives the cluster.)

--- architectures/lib/tpaexec/platform/docker.py
+++ architectures/lib/tpaexec/platform/docker.py
@@ -4,10 +4,15 @@
 
 from tpaexec.platform import Platform
 
-import os, sys
+import os, sys, time
 
 class docker(Platform):
+    def __init__(self, name, arch):
+        super().__init__(name, arch)
+        self.ccache = None
+
     def add_platform_options(self, p, g):
+        g.add_argument('--shared-ccache', metavar='PATH')
         g.add_argument('--local-source-directories', nargs='+', metavar='NAME:PATH')
 
     def validate_arguments(self, args):
@@ -58,6 +63,29 @@ class docker(Platform):
                 print("ERROR: --local-source-directories %s" % (e), file=sys.stderr)
             sys.exit(-1)
 
+        # If we're going to be building anything from source, we set up a shared
+        # ccache. By default, this is a named Docker volume shared between the
+        # containers in the cluster, so that it doesn't affect anything on the
+        # host. Specify --shared-ccache to use a host directory instead.
+
+        if args.get('install_from_source'):
+            ccache = args.get('shared_ccache')
+            if ccache:
+                ccache = os.path.abspath(os.path.expanduser(args.get('shared_ccache')))
+                if not os.path.isdir(ccache):
+                    try:
+                        os.mkdir(ccache)
+                    except Exception as e:
+                        print("ERROR: --shared-ccache: %s" % str(e), file=sys.stderr)
+                        sys.exit(-1)
+            else:
+                # We don't have access to the cluster name here (it's set only
+                # in process_arguments), so we leave a '%s' to be filled in by
+                # update_instance_defaults() below.
+                ccache = 'ccache-%%s-%s' % time.strftime("%Y%m%d%H%M%S", time.localtime())
+
+            self.ccache = '%s:/root/.ccache:rw' % ccache
+
         args['local_sources'] = local_sources
 
     def supported_distributions(self):
@@ -102,6 +130,10 @@ class docker(Platform):
     def update_instance_defaults(self, instance_defaults, args, **kwargs):
         y = self.arch.load_yaml('platforms/docker/instance_defaults.yml.j2', args)
         if y:
+            if self.ccache:
+                sources = y.get('local_source_directories', [])
+                sources.append(self.ccache % args['cluster_name'])
+                y['local_source_directories'] = sources
             instance_defaults.update(y)
 
     def update_instances(self, instances, args, **kwargs):
