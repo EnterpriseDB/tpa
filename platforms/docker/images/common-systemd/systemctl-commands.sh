1a2b0384c1a67ce3c92da1c3b6440b1386d42a00
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2020-09-25 12:15:35 +0530

    Switch to a single-stage build for builtin tpa/xxx docker images
    Martín reported that the build failed if the tpa/xxx image did not
    exist, but the tpa/xxx-base image did (in which case the build step for
    the base image would do/return nothing, but we would still expect it to
    return an image id to pass in as the base image to the next build step).
    
    It would have been possible to sidestep that problem by always removing
    the base image, or check if it existed already, and so on. But William
    suggested that we do a single-stage build for the builtin images, and
    that struck me as a better solution. So:
    
    1. We remove the images/{debian,redhat,ubuntu}-* directories with their
       minimal (and quite similar) Dockerfiles.
    
    2. We add a new _tpa_images table to docker_image.yml that represents
       the same information, i.e., maps from 'label-version' to a literal
       upstream image name, e.g., debian-latest to debian:10.
    
    3. We switch the default build instructions to use a single-stage build
       in images/, using a new Dockerfile that does 'FROM ${BASE_IMAGE}' and
       includes a cut-down version of common-systemd/Dockerfile.
    
    4. Instead of inventing extra build arguments to tell this Dockerfile
       whether it needs to install systemd (on debian/ubuntu) or not (on
       centos), we make common-systemd/systemctl-commands.sh install it.
       (We also deal with some shellcheck complaints in the latter.)
    
    5. We also turn off the default `apt-get upgrade` or `yum update` steps
       for the builtin images to make the build faster. If you really must
       have up-to-date packages, you can still pass in a build context and
       mixin common-systemd in a second build step, as before.
    
    6. For convenience, we also set `pull: yes` by default on the final
       image build, so that it'll pull the upstream image (debian:10 etc.)
       if you don't have the image locally. This has been a frequent source
       of confusion. Set `TPA_DOCKER_PULL_BASE_IMAGE=no` in the environment
       to override (e.g., in CI, where pulling images will probably result
       in transient failures), or set `pull: no` in `docker_images` if you
       are specifying custom build steps.
    
    References: RM19201

--- platforms/docker/images/common-systemd/systemctl-commands.sh
+++ platforms/docker/images/common-systemd/systemctl-commands.sh
@@ -3,6 +3,10 @@ set -e -u -x
 export SYSTEMD_COLORS=0
 source /etc/os-release
 
+if [[ $ID = "debian" || $ID = "ubuntu" ]]; then
+    apt-get update -y && apt-get install -y systemd
+fi
+
 # Don't try to run a full system in the container
 systemctl enable basic.target
 systemctl set-default basic
@@ -10,39 +14,37 @@ systemctl preset-all
 
 installedunits=$( systemctl list-unit-files | head -n -1 | tail -n -1 | cut -f1 -d ' ')
 
-UNITS_TO_DISABLE=" machines.target network-pre.target network.target
-  remote-fs-pre.target remote-fs.target swap.target
-  systemd-hwdb-update.service systemd-readahead-collect.service
-  systemd-readahead-replay.service systemd-readahead-done.timer
-  systemd-readahead-done.service
-  proc-sys-fs-binfmt_misc.mount sys-kernel-config.mount
-  sys-kernel-debug.mount
-  systemd-ask-password-console.path systemd-ask-password-wall.path
+UNITS_TO_DISABLE="
+    machines.target network-pre.target network.target
+    remote-fs-pre.target remote-fs.target swap.target
+    systemd-hwdb-update.service systemd-readahead-collect.service
+    systemd-readahead-replay.service systemd-readahead-done.timer
+    systemd-readahead-done.service
+    proc-sys-fs-binfmt_misc.mount sys-kernel-config.mount
+    sys-kernel-debug.mount
+    systemd-ask-password-console.path systemd-ask-password-wall.path
 "
 
 UNITS_TO_MASK="
-	getty.target console-getty.service container-getty@.service
-	getty-pre.target getty@.service serial-getty@.service
-	time-sync.target
-	system-logind.service
-	sys-fs-fuse-connections.mount
+    getty.target console-getty.service container-getty@.service
+    getty-pre.target getty@.service serial-getty@.service
+    time-sync.target
+    system-logind.service
+    sys-fs-fuse-connections.mount
 "
 
-for target in $UNITS_TO_DISABLE ;
-do
-	# need to check a unit is installed before trying to disable it
-	if [[ " ${installedunits[@]} " =~ " $target " ]] ; then
-		systemctl disable $target
-	fi
+for target in $UNITS_TO_DISABLE; do
+    IFS=' '
+    if [[ " ${installedunits[*]} " =~ " $target " ]] ; then
+        systemctl disable "$target"
+    fi
 done
 
-for target in $UNITS_TO_MASK ;
-do
-	systemctl mask $target
+for target in $UNITS_TO_MASK; do
+    systemctl mask "$target"
 done
 
-if [ $ID = "ubuntu" ]
-then
+if [[ $ID = "ubuntu" ]]; then
     systemctl disable apt-daily-upgrade.timer
     systemctl disable motd-news.timer
 fi
