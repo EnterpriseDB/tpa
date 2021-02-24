0b3860ed119dcb07ddf391a75c24246f46e5d9b1
Richard Poole <richard.poole@2ndQuadrant.com>
2021-02-24 12:32:20 +0000

    Add BDR-Always-ON pool-{enable,disable}-server commands
    We extract some code from the zero-downtime rolling update sequence in
    update-postgres and make standalone commands to add or remove a single
    BDR instance from the haproxy server pool, to perform maintenance in a
    BDR-Always-ON cluster, e.g.
    
        tpaexec pool-disable-server . someinstance
        tpaexec rehydrate . someinstance
        tpaexec pool-enable-server . someinstance
    
    (Note: due to the multiple-play structure of the update-postgres code,
    these commands duplicate the code for now, rather than reuse it.)
    
    Closes #77
    Closes #78

---
+++ architectures/BDR-Always-ON/commands/pool-disable-server.sh
@@ -0,0 +1,30 @@
+# Copyright © 2ndQuadrant Limited <info@2ndquadrant.com>
+
+set -eu
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+    script=$(basename "$0")
+    echo "ERROR: this script is meant to be executed with 'tpaexec ${script%.sh} …'" >&2
+    exit 1
+fi
+
+_tpaexec_command() {
+    instance=${1:?no instance specified}
+    shift
+
+    REMAINDER=()
+    while [[ $# -gt 0 ]]; do
+        opt=$1
+        shift
+
+        case "$opt" in
+            *)
+                REMAINDER+=("$opt")
+                ;;
+        esac
+    done
+
+    set -- "${REMAINDER[@]:+${REMAINDER[@]}}" "$@"
+
+    time playbook "$TPA_DIR/architectures/BDR-Always-ON/commands/pool-disable-server.yml" -e disable_instance="$instance" "$@"
+}
