6b2d0b1da8bea5bab5b9082a889add1ff28b2c8f
Muhammad Haroon <muhammad.haroon@enterprisedb.com>
2021-03-30 23:52:45 -0700

    Move show-password and store-password into lib/commands
    Now that we have the ability to run custom commands and display help, we
    don't need to put everything into tpaexec binary any more.
    
    Fixes: #143
    
    Signed-off-by: Muhammad Haroon <muhammad.haroon@enterprisedb.com>

---
+++ architectures/lib/commands/show-password.sh
@@ -0,0 +1,42 @@
+# Copyright © EnterpriseDB Corporation
+
+set -eu
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+    script=$(basename "$0")
+    echo "ERROR: this script is meant to be executed with 'tpaexec ${script%.sh} …'" >&2
+    exit 1
+fi
+
+_tpaexec_command() {
+    args=()
+
+    username=${1:?No username specified}
+    shift
+
+    if [ -f vault/vault_pass.txt ]; then
+        args+=(--vault-password-file vault/vault_pass.txt)
+    fi
+
+    dir=""
+    if [[ -d ./inventory ]]; then
+        dir=$(find ./inventory -path '*/group_vars/*/secrets' -type d)
+    fi
+    if [[ -z "$dir" ]]; then
+        error "inventory directory does not exist (did you run tpaexec provision?)"
+    fi
+    file="$dir"/"$username"_password.yml
+    if [[ ! -f $file && -f "$dir/$username".yml ]]; then
+        file="$dir"/"$username".yml
+    fi
+
+    "$ansible"-vaultpw "${args[@]}" show "$file" "$@"
+}
+
+_tpaexec_help() {
+    cat <<HELP
+Command: tpaexec show-password clustername username
+
+Shows the password stored in the local inventory for the given user.
+HELP
+}
---
+++ architectures/lib/commands/store-password.sh
@@ -0,0 +1,71 @@
+# Copyright © EnterpriseDB Corporation
+
+set -eu
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+    script=$(basename "$0")
+    echo "ERROR: this script is meant to be executed with 'tpaexec ${script%.sh} …'" >&2
+    exit 1
+fi
+
+_tpaexec_command() {
+    args=()
+    while [[ $# -gt 0 ]]; do
+        opt=$1
+        shift
+
+        case "$opt" in
+            --secret-name)
+                secret_name=${1:?secret name not specified}
+                shift
+                ;;
+            --random)
+                args+=(-c "$TPA_DIR"/architectures/lib/password)
+                ;;
+            --)
+                break
+                ;;
+            *)
+                REMAINDER+=("$opt")
+                ;;
+        esac
+    done
+
+    set -- "${REMAINDER[@]:+${REMAINDER[@]}}" "$@"
+
+    if [[ ${secret_name:-''} == "" ]]; then
+        username=${1:?No username specified}
+        secret_name="$username"_password
+        shift
+    fi
+
+    if [ -f vault/vault_pass.txt ]; then
+        args+=(--vault-password-file vault/vault_pass.txt)
+    fi
+
+    dir=""
+    if [[ -d ./inventory ]]; then
+        dir=$(find ./inventory -path '*/group_vars/*/secrets' -type d)
+    fi
+    if [[ -z "$dir" ]]; then
+        error "inventory directory does not exist (did you run tpaexec provision?)"
+    fi
+    file="$dir"/"$secret_name".yml
+
+    "$ansible"-vaultpw "${args[@]}" store "$file" "$@"
+
+}
+
+_tpaexec_help() {
+    cat <<HELP
+Command: tpaexec store-password clustername username [--random]
+
+Stores an encrypted new password for the given user in the inventory.
+
+Prompts you to enter a new password by default, but will generate and
+use a random password instead if you specify --random.
+
+Note: the new password is only stored in the local inventory. It will be
+changed on the target instances (if needed) only during the next deploy.
+HELP
+}
