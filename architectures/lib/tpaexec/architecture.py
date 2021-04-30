027102fb13b82c61ed3131633ca5fb6e4b5c916c
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2021-04-30 17:40:36 +0530

    Add bdr_enterprise_3_7-epas repository too

--- architectures/lib/tpaexec/architecture.py
+++ architectures/lib/tpaexec/architecture.py
@@ -508,7 +508,7 @@ class Architecture(object):
             'default', '2ndqpostgres', 'bdr2', 'bdr3', 'pglogical3',
             'server-ssl-passphrase-callback', 'postgresql', 'bdr_enterprise_3_6',
             'livecompare', 'bdr3_7', 'pglogical3_7', 'bdr_enterprise_3_7', 'harp',
-            'bdr3_7-epas'
+            'bdr3_7-epas', 'bdr_enterprise_3_7-epas',
         ]
 
     # Returns a map of things that should be accepted as arguments to
