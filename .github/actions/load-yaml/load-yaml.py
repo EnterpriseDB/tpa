1666505ec7a0c21694c6f079a16830b12670fb59
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2022-04-20 20:00:49 +0530

    Apply black formatting fixes

--- .github/actions/load-yaml/load-yaml.py
+++ .github/actions/load-yaml/load-yaml.py
@@ -9,9 +9,13 @@ import json
 
 
 def get_args():
-    parser = argparse.ArgumentParser('load-yaml', description="Load a YAML file for input to Github action")
-    parser.add_argument('data', metavar='FILE', type=read_data, help='Path to YAML file')
-    parser.add_argument('key', nargs="?", help='Optional top level key name to return')
+    parser = argparse.ArgumentParser(
+        "load-yaml", description="Load a YAML file for input to Github action"
+    )
+    parser.add_argument(
+        "data", metavar="FILE", type=read_data, help="Path to YAML file"
+    )
+    parser.add_argument("key", nargs="?", help="Optional top level key name to return")
     return parser.parse_args()
 
 
@@ -23,8 +27,8 @@ def read_data(file_name):
 def main():
     args = get_args()
     data = args.data[args.key] if args.key else args.data
-    print(f'::set-output name=json::{json.dumps(data)}')
+    print(f"::set-output name=json::{json.dumps(data)}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
