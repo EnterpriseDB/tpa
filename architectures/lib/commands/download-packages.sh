b69f5ce96020ae704ed73946250ae6d798300e9c
Matt Baker <matt.baker@enterprisedb.com>
2022-06-09 14:27:43 +0100

    Add --os and --os-version args to download-packages

--- architectures/lib/commands/download-packages.sh
+++ architectures/lib/commands/download-packages.sh
@@ -17,10 +17,17 @@ _tpaexec_command() {
         shift
 
         case "$opt" in
+            --os)
+                image_os=${1:?"docker image OS not specified"}
+                shift
+                ;;
+            --os-version)
+                os_version=${1:?"docker image OS version not specified"}
+                shift
+                ;;
             --docker-image)
                 docker_image=${1:?"docker image not specified"}
                 shift
-                OPTS+=('-e' "default_docker_image=$docker_image")
                 ;;
             --download-dir)
                 download_dir=${1:?"path to download directory not provided"}
@@ -39,6 +46,11 @@ _tpaexec_command() {
     done
 
     set -- "${REMAINDER[@]:+${REMAINDER[@]}}" "$@"
+
+    # $docker_image takes priority, if that's not set construct the image name from
+    # image_os (lower case) and os_version (if set)
+    docker_image="${docker_image:-${image_os:+tpa/${image_os,,}}${os_version:+:${os_version}}}"
+    OPTS+=('-e' "default_docker_image=$docker_image")
 
     if [[ -z ${docker_image+x} ]]; then
         echo "Please specify an image for the package download container with --docker-image" >&2
