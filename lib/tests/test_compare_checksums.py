9c2e70cfde7538559dac71d993a5029999cfe156
John Nevin <john.nevin@enterprisedb.com>
2023-05-10 09:37:13 -0400

    Add support for TPA source validation
    Since TPA install includes source for playbooks, python and other
    scripts, we have seen that an intentional or unintentional attempt to
    change the source of the playbooks, python or included scripts can cause
    TPA to fail in ways that are extremely difficult to debug.
    
    This change includes a simple verification mechanism as part of `tpaexec
    info` command that performs a validation check on TPA installation and
    reports if any files fail to match the checksum distributed as part of
    TPA package[1]. For source repository clones (strictly allowed for
    internal use only), it relies on `git` to validate the source.
    
    This is a weak defence designed to spot changes made without malice. It
    is not intended to protects against malicious alterations to the code.
    As it only runs as part of `tpaexec info` command, it does not provide
    any runtime protection.
    
    [1] Includes a corresponding change in tpaexec-packaging repository as
    part of d927f14dbc
    
    References: TPA-322

---
+++ lib/tests/test_compare_checksums.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# © Copyright EnterpriseDB UK Limited 2015-2023 - All rights reserved.
+
+import pytest
+
+from pathlib import Path
+from tpaexec.compare_checksums import (
+    _hash_file,
+    _git_status,
+    compare_data,
+    get_args,
+)
+
+
+@pytest.fixture(scope="session")
+def setup():
+    directory = "lib/tests/checksums/test_directory"
+    tampered_missing_directory = "lib/tests/checksums/tampered_missing_test_directory"
+    tampered_nested_directory = "lib/tests/checksums/tampered_nested_test_directory"
+    paths = list(Path(directory).glob("**/*"))
+    extensions = [".json", ".txt", ".yml"]
+    source_checksums = "lib/tests/checksums/checksums.json"
+    nonexistent_checksums = "lib/tests/checksums/nonexistent_checksums.json"
+
+    return locals()
+
+
+def test_get_args(setup):
+    args = get_args(args=[setup["directory"], setup["source_checksums"]])
+    assert args.directory == setup["directory"]
+    assert args.checksums_file == setup["source_checksums"]
+
+
+def test_hash_file(setup):
+    for path in setup["paths"]:
+        if path.is_file() and path.exists():
+            checksum = _hash_file(path_to_file=path)
+            assert len(checksum.digest()) == 32
+            assert len(checksum.hexdigest()) == 64
+
+
+def test_git_status():
+    with pytest.raises(SystemExit) as exit:
+        _git_status()
+    assert exit.type == SystemExit
+
+
+def test_compare_data(setup):
+    mismatch_list, missing_list = compare_data(
+        path_to_file=setup["source_checksums"], target_directory=setup["directory"]
+    )
+    assert len(mismatch_list) == 0
+    assert len(missing_list) == 0
+
+    mismatch_list, missing_list = compare_data(
+        path_to_file=setup["source_checksums"],
+        target_directory=setup["tampered_nested_directory"],
+    )
+    assert len(mismatch_list) == 4
+    assert len(missing_list) == 0
+
+    mismatch_list, missing_list = compare_data(
+        path_to_file=setup["source_checksums"],
+        target_directory=setup["tampered_missing_directory"],
+    )
+    assert len(mismatch_list) == 1
+    assert len(missing_list) == 4
+
+    with pytest.raises(SystemExit) as exit:
+        compare_data(
+            path_to_file=setup["nonexistent_checksums"],
+            target_directory=setup["directory"],
+        )
+    assert exit.type == SystemExit
---
+++ lib/tpaexec/compare_checksums.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# © Copyright EnterpriseDB UK Limited 2015-2023 - All rights reserved.
+
+import argparse
+import hashlib
+import json
+import sys
+import subprocess
+from pathlib import Path
+
+
+def get_args(args=None):
+    parser = argparse.ArgumentParser(
+        "create checksums",
+        description="Load a directory of files to create checksum hashes for each one",
+    )
+    parser.add_argument(
+        "directory", metavar="PATH", help="Path to directory containing files"
+    )
+    parser.add_argument(
+        "checksums_file",
+        metavar="SAVE_FILE",
+        help="Path to the JSON file to which file checksum data is written",
+    )
+    return parser.parse_args(args)
+
+
+def _hash_file(path_to_file: Path):
+    sha256_hash = hashlib.sha256(path_to_file.read_bytes())
+    return sha256_hash
+
+
+def _git_status():
+    try:
+        result = subprocess.run(["git", "status"], stdout=subprocess.PIPE, check=True)
+        print(result.stdout.decode("utf-8"))
+        sys.exit(0)
+    except subprocess.CalledProcessError:
+        print(
+            "Unable to compare checksums!\n'checksums.json' file not found, and 'git status' could not be run."
+        )
+        sys.exit(1)
+
+
+def compare_data(path_to_file: str, target_directory: str):
+    mismatch_list = []
+    missing_list = []
+    try:
+        with open(path_to_file, "r", encoding="utf-8") as in_file:
+            source_checksums = json.load(in_file)
+            for source_filepath, source_hash_value in source_checksums.items():
+                target_filepath = Path.joinpath(Path(target_directory), source_filepath)
+                if target_filepath.exists() and target_filepath.is_file():
+                    calculated_hash_value = _hash_file(target_filepath).hexdigest()
+
+                    if calculated_hash_value != source_hash_value:
+                        mismatch_list.append(f"{source_filepath}")
+                else:
+                    missing_list.append(f"{source_filepath}")
+
+    except IOError:
+        _git_status()
+
+    return mismatch_list, missing_list
+
+
+if __name__ == "__main__":  # pragma: no cover
+    args = get_args()
+    mismatch_list, missing_list = compare_data(
+        path_to_file=args.checksums_file, target_directory=args.directory
+    )
+    if len(mismatch_list) == 0:
+        print(f"{_hash_file(path_to_file=Path(args.checksums_file)).hexdigest()} [OK]")
+    else:
+        print(*mismatch_list, sep=", ")
