abe772c0c031e49611eb1964672fb7ab2b99f09c
Richard Poole <richard.poole@enterprisedb.com>
2023-12-13 18:43:13 +0000

    Respect package versions in the downloader
    When using the downloader on a Debian-family system, we now perform our
    own fnmatch-style globbing on any package versions specified in
    config.yml, enabling constructions like `bdr_package_version: 4:5.0.*`
    to behave in the same way as when the downloader is not in use.
    
    Fixes: TPA-463
    Fixes: TPA-583

---
+++ lib/filter_plugins/fnmatch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# © Copyright EnterpriseDB UK Limited 2015-2023 - All rights reserved.
+
+from typing import Dict, List, Any
+import fnmatch as fnm
+
+def fnmatch(string: str, pattern: str) -> bool:
+    """Given a package version as returned by apt-cache, return True iff it
+    matches the package_spec
+    """
+    if (fnm.fnmatch(string, pattern)):
+        return True
+    return False
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            "fnmatch": fnmatch,
+        }
