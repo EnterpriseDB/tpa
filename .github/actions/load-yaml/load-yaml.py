00115478b1af9bd50db3d0b8def4d8b05241bc5a
Muhammad Haroon <muhammad.haroon@enterprisedb.com>
2024-01-09 13:24:13 +0500

    Add GH workflows and integration tests
    In a recent shuffle to make TPA public, integration tests were moved out
    of the main TPA source repository to restrict access. However, now that
    we want to move to a public/private repo pair for TPA development for
    various reasons, it makes sense to add workflows and integration tests
    to the private repo to make development testing easier.
    
    Signed-off-by: Muhammad Haroon <muhammad.haroon@enterprisedb.com>

---
+++ .github/actions/load-yaml/load-yaml.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#  © Copyright EnterpriseDB UK Limited 2015-2023 - All rights reserved.
+"""Load a YAML file to use as input for a Github action."""
+
+import argparse
+import os
+
+import yaml
+import json
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        "load-yaml", description="Load a YAML file for input to Github action"
+    )
+    parser.add_argument(
+        "data", metavar="FILE", type=read_data, help="Path to YAML file"
+    )
+    parser.add_argument("key", nargs="?", help="Optional top level key name to return")
+    return parser.parse_args()
+
+
+def read_data(file_name):
+    with open(file_name) as fh:
+        return yaml.safe_load(fh)
+
+
+def set_output(data):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as output_fh:
+        output_fh.write(f'json={json.dumps(data)}\n')
+
+
+def main():
+    args = get_args()
+    data = args.data[args.key] if args.key else args.data
+    set_output(data)
+
+
+if __name__ == "__main__":
+    main()
